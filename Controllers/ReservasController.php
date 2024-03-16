a8160d669dc98ab08e780c16bcdcf81b4e855a01
JeffersonAlves7 <ilj031420@hotmail.com>
2024-03-15 22:14:51 -0300

    feat/reserves: add stock filter in reserves page

--- Controllers/ReservasController.php
+++ Controllers/ReservasController.php
@@ -1,9 +1,24 @@
 <?php
-
+require_once "Models/Estoque.php";
 class ReservasController
 {
+    private $estoquesModel = null;
+
+    public function __construct()
+    {
+        $this->estoquesModel = new Estoque();
+    }
+
     public function index()
     {
+        if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["name"])) {
+            $name = $_POST["name"];
+            $this->estoquesModel->create($name);
+            header("location: /estoques");
+        }
+
+        $stocks = $this->estoquesModel->getAll();
+
         include_once "Views/Reservas.php";
     }
 }
--- Views/Reservas.php
+++ Views/Reservas.php
@@ -7,10 +7,31 @@ ob_start();
 
 <div class="container">
     <h1 class="mt-4 mb-3"><?php echo $pageTitle ?></h1>
+
+    <!-- Estoques -->
+    <div class="d-flex gap-3">
+        <form method="get">
+            <button type="submit" class="btn btn-custom <?php echo isset($_GET["estoque"]) ? "" : "active" ?>">Geral</button>
+        </form>
+
+        <?php
+        if (isset($stocks) && $stocks->num_rows > 0) {
+            while ($estoque = $stocks->fetch_assoc()) {
+                $name = $estoque["name"];
+                $ID = $estoque["ID"];
+                $active = (isset($_GET["estoque"]) && $_GET["estoque"] == "$ID" ? "active" : "");
+
+                echo "<form method='get'>
+                    <input type='hidden' name='estoque' value='$ID'/>
+                    <button type='submit' class='btn btn-custom $active'>$name</button>
+                </form>";
+            }
+        }
+        ?>
+    </div>
 </div>
 
 <?php
 $content = ob_get_clean();
 include "Components/Template.php";
 ?>
-
