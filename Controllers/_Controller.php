d4e04e06832df7174332d635756edc8a7af010b4
JeffersonAlves7 <ilj031420@hotmail.com>
2024-04-04 20:08:30 -0300

    Refactor session handling in _Controller class

--- Controllers/_Controller.php
+++ Controllers/_Controller.php
@@ -1,7 +1,18 @@
 <?php
 
-class _Controller {
-    public function __construct() {
+class _Controller
+{
+    public function __construct()
+    {
+        if (!isset($_SESSION)) {
+            session_start();
+        }
+
+        $this->verifySession();
+    }
+
+    private function verifySession()
+    {
         if (!isset($_SESSION["username"]) && !isset($_SESSION["email"])) {
             header("location: /auth/login");
             exit(0);
