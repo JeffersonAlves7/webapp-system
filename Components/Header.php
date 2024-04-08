78680d03a709da3d50d5145849176ff92d5c53b7
JeffersonAlves7 <ilj031420@hotmail.com>
2024-04-08 13:50:47 -0300

    Update header and navigation styles

--- Components/Header.php
+++ Components/Header.php
@@ -1,41 +1,42 @@
 <link rel="stylesheet" href="/public/grid-template.css">
 
 <header>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light p-0">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="/">
-        ATTUS
-      </a>
+  <nav class="navbar navbar-expand-md p-0">
+    <div id="logoArea">
+      <a class="navbar-brand p-0 m-0" href="#">ATTUS SYSTEM</a>
 
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link" href="/auth/info">
-              <i class="bi bi-person-circle"></i>
-              <?= $_SESSION["username"]; ?>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/auth/logout">
-              <i class="bi bi-box-arrow-right"></i>
-              Sair
-            </a>
-          </li>
-        </ul>
-      </div>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    </div>
+
+    <div class="collapse navbar-collapse w-100" id="navbarSupportedContent">
+      <ul class="navbar-nav mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link <?= (isset($pageTitle) && $pageTitle === "Estoques") ? 'active' : ''; ?>" href="/">Estoques</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link <?= (isset($pageTitle) && $pageTitle === "Relatórios") ? 'active' : ''; ?>" href="#">Relatórios</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link <?= (isset($pageTitle) && $pageTitle === "Incluir Lançamento") ? 'active' : ''; ?>" href="/lancamento">Incluir Lançamento</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link <?= (isset($pageTitle) && $pageTitle === "Embarques") ? 'active' : ''; ?>" href="/embarques">Embarques</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link <?= (isset($pageTitle) && $pageTitle === "Produtos") ? 'active' : ''; ?>" href="/produtos">Lista de produtos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link <?= (isset($pageTitle) && $pageTitle === "Reservas") ? 'active' : ''; ?>" href="/reservas">Reservas</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/auth/info">
+            <i class="bi bi-person-circle"></i>
+            <?= $_SESSION["username"]; ?>
+          </a>
+        </li>
+      </ul>
     </div>
   </nav>
 </header>
-
-<aside class="p-3">
-  <nav>
-    <ul class="nav flex-column flex-sm-row">
-      <li class="nav-item"><a class="nav-link <?= (isset($pageTitle) && $pageTitle === "Estoques") ? 'active' : ''; ?>" href="/">Estoques</a></li>
-      <li class="nav-item"><a class="nav-link <?= (isset($pageTitle) && $pageTitle === "Relatórios") ? 'active' : ''; ?>" href="#">Relatórios</a></li>
-      <li class="nav-item"><a class="nav-link <?= (isset($pageTitle) && $pageTitle === "Incluir Lançamento") ? 'active' : ''; ?>" href="/lancamento">Incluir Lançamento</a></li>
-      <li class="nav-item"><a class="nav-link <?= (isset($pageTitle) && $pageTitle === "Embarques") ? 'active' : ''; ?>" href="/embarques">Embarques</a></li>
-      <li class="nav-item"><a class="nav-link <?= (isset($pageTitle) && $pageTitle === "Produtos") ? 'active' : ''; ?>" href="/produtos">Lista de produtos</a></li>
-      <li class="nav-item"><a class="nav-link <?= (isset($pageTitle) && $pageTitle === "Reservas") ? 'active' : ''; ?>" href="/reservas">Reservas</a></li>
-    </ul>
-  </nav>
-</aside>
