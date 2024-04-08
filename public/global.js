d498c213ab02cd01af6a13828fd4bd9125072cc1
JeffersonAlves7 <ilj031420@hotmail.com>
2024-04-08 17:35:26 -0300

    feat add toggle description function inside global js

--- public/global.js
+++ public/global.js
@@ -3,3 +3,11 @@ const buttonToGoBack = document.getElementById("go-back");
 buttonToGoBack?.addEventListener("click", () => {
   window.history.back();
 });
+
+document.querySelectorAll(".toggle-description").forEach(function (element) {
+  element.addEventListener("click", function (event) {
+    event.preventDefault();
+    var fullDescription = this.getAttribute("data-full-description");
+    alert(fullDescription);
+  });
+});
