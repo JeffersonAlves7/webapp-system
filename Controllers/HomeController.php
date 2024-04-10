4b6568fe64fd4f08ff115545bbdfc9e56f921ee2
Jefferson <90939375+JeffersonAlves7@users.noreply.github.com>
2024-04-10 20:26:26 -0300

    Refactor controllers and create EstoquesController

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -1,58 +1,7 @@
 <?php
 
-require_once "Models/Estoque.php";
-require_once "Controllers/_Controller.php";
+require_once "Controllers/EstoquesController.php";
 
-class HomeController extends _Controller
+class HomeController extends EstoquesController
 {
-    private $estoquesModel;
-
-    public function __construct()
-    {
-        parent::__construct();
-        $this->estoquesModel = new Estoque();
-    }
-
-    public function index()
-    {
-        // if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["name"])) {
-        //     $name = $_POST["name"];
-        //     $this->estoquesModel->create($name);
-        //     header("location: /estoques");
-        // }
-        
-        $page = 1;
-
-        if (isset($_GET["page"])) {
-            $page = $_GET["page"];
-        }
-
-        $estoque_ID = null;
-        if(isset($_GET["estoque"]) && $_GET["estoque"] != "") {
-            $estoque_ID = $_GET["estoque"];
-        }
-
-        $stocks = $this->estoquesModel->getAll();
-        $productsData = $this->estoquesModel->getProductsByStock($estoque_ID, $page);
-        $produtos = $productsData["products"];
-        $pageCount = $productsData["pageCount"];
-
-        include_once "Views/Estoques.php";
-    }
-
-    public function getAll()
-    {
-        header("Content-type: application/json");
-
-        $stocks = $this->estoquesModel->getAll();
-        $stocks_arr = array();
-        if ($stocks->num_rows > 0) {
-            while ($stock = $stocks->fetch_assoc()) {
-                array_push($stocks_arr, $stock);
-            }
-        }
-
-        echo json_encode(array("stocks" => $stocks_arr));
-        exit(0);
-    }
 }
