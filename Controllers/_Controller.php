880855abbaf957ab6d4730a5697166cebb7ea1cc
JeffersonAlves7 <ilj031420@hotmail.com>
2024-07-14 17:51:49 -0300

    refactor: Add permission verification methods to _Controller class

--- Controllers/_Controller.php
+++ Controllers/_Controller.php
@@ -19,4 +19,48 @@ class _Controller extends _Viewer
         AuthManager::checkLogin();
         $this->userPermissionManager = new UserPermissionManager($user['id']);
     }
+
+    protected function verifyWritePermission($controller_name)
+    {
+        if (
+            !$this->userPermissionManager->controller($controller_name)->canWrite()
+        ) {
+            $_SESSION['mensagem_erro'] = "Você não tem permissão para realizar esta ação!";
+            header("Location: /produtos");
+            exit;
+        }
+    }
+
+    protected function verifyDeletePermission($controller_name)
+    {
+        if (
+            !$this->userPermissionManager->controller($controller_name)->canDelete()
+        ) {
+            $_SESSION['mensagem_erro'] = "Você não tem permissão para realizar esta ação!";
+            header("Location: /produtos");
+            exit;
+        }
+    }
+
+    protected function verifyReadPermission($controller_name)
+    {
+        if (
+            !$this->userPermissionManager->controller($controller_name)->canRead()
+        ) {
+            $_SESSION['mensagem_erro'] = "Você não tem permissão para acessar esta página!";
+            header("Location: /");
+            exit;
+        }
+    }
+
+    protected function verifyEditPermission($controller_name)
+    {
+        if (
+            !$this->userPermissionManager->controller($controller_name)->canEdit()
+        ) {
+            $_SESSION['mensagem_erro'] = "Você não tem permissão para realizar esta ação!";
+            header("Location: /produtos");
+            exit;
+        }
+    }
 }
